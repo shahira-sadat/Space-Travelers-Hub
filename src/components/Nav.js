87113c7a447c0b509c364da20862067909dc8d3c
Muhyideen Elias <emuhiydeen6@gmail.com>
2022-08-23 03:24:25 +0000

    add nav logo and header title

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,11 +2,13 @@ import logo from '../assets/images/planet.png';
 
 const Nav = () => (
   <>
-    <header>
-      <img src={logo} alt="logo" />
-      {/* eslint-disable-next-line react/no-unescaped-entities */}
-      <h1>Space Travelers' Hub</h1>
-    </header>
+    <nav className="nav--bar">
+      <div className="nav--bar--logo">
+        <img className="logo--img" src={logo} alt="logo" />
+        {/* eslint-disable-next-line react/no-unescaped-entities */}
+        <h1 className="nav--title">Space Travelers' Hub</h1>
+      </div>
+    </nav>
   </>
 );
 
