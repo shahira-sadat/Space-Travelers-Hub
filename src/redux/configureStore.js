c37167fb0ce93e99a25dd48bf03e8d305a9bb667
shahira-sadat <shahira.sadat1@gmail.com>
2022-08-24 12:28:12 +0430

    Update configureStore

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,12 +1,12 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
-import missions from './missions/missions';
-import rockets from './rockets/rockets';
+import missionsReducer from './missions/missions';
+import rocketsReducer from './rockets/rockets';
 
 const reducer = combineReducers({
-  missions,
-  rockets,
+  missionsReducer,
+  rocketsReducer,
 });
 
 const store = createStore(
