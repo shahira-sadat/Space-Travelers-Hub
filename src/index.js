6e956d532367e55dbb6f0ceae2c1bc3849e6b6a2
shahira-sadat <shahira.sadat1@gmail.com>
2022-08-24 12:27:01 +0430

    Add bootstrap

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './redux/configureStore';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
