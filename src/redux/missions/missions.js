4c06a8c059d6e2705f6def7c43857eeef4aef6cc
shahira-sadat <shahira.sadat1@gmail.com>
2022-08-24 12:29:36 +0430

    Add redux/missions action and reducers

--- src/redux/missions/missions.js
+++ src/redux/missions/missions.js
@@ -1,8 +1,35 @@
-import React from 'react';
+import getMissions from '../../getMissions';
 
-const missions = () => (
-  <div>
-    <h1>Under Construction</h1>
-  </div>
-);
-export default missions;
+const LOAD_MISSIONS = 'space-travelers-hub/missions/LOAD_MISSIONS';
+
+const initialState = [];
+
+export const loadMissions = () => async (dispatch) => {
+  const getResult = await getMissions();
+  const missions = getResult.map((mission) => ({
+    id: mission.mission_id,
+    name: mission.mission_name,
+    description: mission.description,
+  }));
+
+  if (missions) {
+    dispatch({
+      type: LOAD_MISSIONS,
+      payload: missions,
+    });
+  }
+};
+
+const missionsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case LOAD_MISSIONS:
+      return {
+        ...state,
+        missions: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default missionsReducer;
