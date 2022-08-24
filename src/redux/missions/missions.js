b2d7a701bca1dc3971084e007cfd8883e553fba8
Muhyideen Elias <emuhiydeen6@gmail.com>
2022-08-24 08:10:40 +0000

    add implementation for redux state slice for missions and implement configureStore

--- src/redux/missions/missions.js
+++ src/redux/missions/missions.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import axios from 'axios';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
-const missions = () => (
-  <div>
-    <h1>Under Construction</h1>
-  </div>
+const apiMissions = 'https://api.spacexdata.com/v3/rockets';
+
+const FETCH_MISSIONS = 'space-travelers-hub/missions/FETCH_MISSIONS';
+// const GET_MISSIONS = 'space-travelers-hub/missions/GET_MISSIONS';
+export const fetchMissions = createAsyncThunk(
+  FETCH_MISSIONS,
+  async () => {
+    const response = await axios.get(apiMissions);
+    return response.data;
+  },
 );
-export default missions;
+
+const storeSlice = createSlice({
+  name: 'space-travelers-hub/missions',
+  initialState: [],
+  extraReducers: {
+    [fetchMissions.fulfilled]: (state, action) => action.payload,
+  },
+});
+
+export default storeSlice.reducer;
